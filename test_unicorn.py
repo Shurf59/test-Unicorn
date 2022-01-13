9a17bf8109e8f56d81788acf78bebdea37372893
Shurf59 <94610906+Shurf59@users.noreply.github.com>
2022-01-13 21:02:44 +0500

    Add files via upload

--- test_unicorn.py
+++ test_unicorn.py
@@ -1,10 +1,11 @@
 import logging
+import aiohttp
 from aiohttp import web
 import requests
 import json
 import argparse
 import time
-from threading import Thread
+import asyncio
 from abstract import TestAbstractClass
 
 
@@ -19,7 +20,7 @@ class Finance:
 
 class Wfo(TestAbstractClass):
 
-    def sum_wallet():
+    async def sum_wallet():
         """Считает сумму всех средств в каждой из валют. Возвращает список сумм по порядку."""
         rub = round(Finance.wallet['rub'] + (Finance.wallet['usd'] * Finance.exchange_rates['rub-usd']) + (
                 Finance.wallet['eur'] * Finance.exchange_rates['rub-eur']), 2)
@@ -29,7 +30,7 @@ class Wfo(TestAbstractClass):
                 Finance.wallet['rub'] / Finance.exchange_rates['rub-eur']), 2)
         return f'sum: {rub} rub / {usd} usd / {eur} eur'
 
-    def financial_data():
+    async def financial_data():
         """Последовательно выводит: список валют в кошельке и их значение, курс валют, сумму всех средств в каждой валюте"""
         wallet_data = ''
         for key, value in Finance.wallet.items():
@@ -38,10 +39,10 @@ class Wfo(TestAbstractClass):
         for key, value in Finance.exchange_rates.items():
             wallet_data += f" {key}: {value}\n"
         wallet_data += f"\n"
-        wallet_data += Wfo.sum_wallet()
+        wallet_data += await Wfo.sum_wallet()
         return wallet_data
 
-    def update_exchange_rates(period_rates):
+    async def update_exchange_rates(period_rates):
         """Выполняет 1 пункт тз. На вход получает период обновления в секундах.
         Парсит курс валют на текущий момент и добовляет данные в словарь."""
         while True:
@@ -59,9 +60,9 @@ class Wfo(TestAbstractClass):
             Finance.exchange_rates['rub-eur'] = data_rate['EUR']
             Finance.exchange_rates['usd-eur'] = round(data_rate['EUR'] / data_rate['USD'], 2)
             logging.info('Данные курса валют успешно обновлены.')
-            time.sleep(period_rates)
+            await asyncio.sleep(period_rates)
 
-    def change_control_finance(period_wallet):
+    async def change_control_finance(period_wallet):
         """Функция для реализации условий 5 пункта тз.
           На вход получает период обновления. Сравнивает текущие значения в кошелька и курса валют с данными из
           локальных словарей. если значения отличаются, то обновляет локальные словари и Последовательно выводит в консоль:
@@ -71,13 +72,15 @@ class Wfo(TestAbstractClass):
         test_wallet = {}
         test_exchange_rates = {}
         while True:
-            time.sleep(period_wallet)
+            # await time.sleep(period_wallet)
             if Finance.wallet != test_wallet or Finance.exchange_rates != test_exchange_rates:
                 for key, value in Finance.wallet.items():
                     test_wallet[key] = value
                 for key, value in Finance.exchange_rates.items():
                     test_exchange_rates[key] = value
-                logging.info(f'\n{Wfo.financial_data()}')
+                result = await Wfo.financial_data()
+                logging.info(f'\n{result}')
+            await asyncio.sleep(period_wallet)
 
     """Все что ниже, удовлетворяет требования 4 пункта тз. Запуск локального сервера, обработка post и get запросов"""
 
@@ -85,7 +88,8 @@ class Wfo(TestAbstractClass):
         """Обробатывает гет запрос и вы водит на веб страницу: список валют в кошельке и их значение,
          курс валют, сумму всех средств в каждой валюте """
         logging.debug(request)
-        response = web.Response(text=Wfo.financial_data())
+        data = await Wfo.financial_data()
+        response = web.Response(text=data)
         logging.debug(response)
         return response
 
@@ -125,6 +129,7 @@ class Wfo(TestAbstractClass):
                 return response
             else:
                 Finance.wallet[key] = value
+        return web.json_response({"status":"ok"})
 
     async def post_change_wallet(request):
         """Обробатывает post запрос с входящим json и суммирует
@@ -137,7 +142,7 @@ class Wfo(TestAbstractClass):
                 Finance.wallet[key] = 0
             else:
                 Finance.wallet[key] += value
-
+        return web.json_response({"status":"ok"})
 
 """Получает аргументы для 1,2,3 пунктов тз."""
 parser = argparse.ArgumentParser()
@@ -169,13 +174,9 @@ elif args.debug in ('0', 'true', 'True', 'y', 'Y'):
 else:
     print('Не верное значение параметра --debug')
 
-logging.info('Старт потоков.')
-flow_update_exchange_rates = Thread(target=Wfo.update_exchange_rates, args=(args.period * 60,), daemon=True)
-flow_change_control_finance = Thread(target=Wfo.change_control_finance, args=(60,), daemon=True)
-flow_update_exchange_rates.start()
-flow_change_control_finance.start()
 
-app = web.Application()
+
+app = aiohttp.web.Application()
 app.add_routes([
     web.get('/amount/get', Wfo.get_amount),
     web.get('/rub/get', Wfo.get_rub),
@@ -184,6 +185,19 @@ app.add_routes([
     web.post('/amount/set', Wfo.post_wallet),
     web.post('/modify', Wfo.post_change_wallet)
 ])
+async def test():
+    task_update_exchange_rates = who_loop.create_task(Wfo.update_exchange_rates(args.period * 60))
+    task_change_control_finance = who_loop.create_task(Wfo.change_control_finance(60))
+    await asyncio.wait([task_update_exchange_rates,task_change_control_finance])
 
-logging.info('Старт локального сервера.')
-web.run_app(app, host='0.0.0.0', port=8080)
+async def run():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8080)
+    await site.start()
+    return await test()
+
+if __name__ == '__main__':
+    who_loop = asyncio.get_event_loop()
+    who_loop.run_until_complete(run())
+
